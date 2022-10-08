2e7262b3fe4738c002f2f748858191ce2d0dd46a
Tatyana787 <stadnikt@gmail.com>
2022-10-08 21:28:47 +0200

    comment

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,12 +1,12 @@
-import React from "react";
-export default function WeatherIcon(props){
-    //console.log(props.data);
-    let iconEl = props.weather[0].icon;
-    let icon = props.data.icon;
-    //icon = document.querySelector("img.current-weather-icon");
+// import React from "react";
+// export default function WeatherIcon(props){
+//     //console.log(props.data);
+//     let iconEl = props.weather[0].icon;
+//     let icon = props.data.icon;
+//     //icon = document.querySelector("img.current-weather-icon");
   
-    return(
-    icon.setAttribute("src", `img/${iconEl}.png`)
+//     return(
+//     icon.setAttribute("src", `img/${iconEl}.png`)
  
-    );
-}
+//     );
+// }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 import'./index.css';
-import WeatherIcon from "./WeatherIcon";
+//import WeatherIcon from "./WeatherIcon";
 // import FormattedDate from "./FormattedDate";
 
 
